a7cdd53d333c28e765b15a7dc1d85aa9d158e1ae
Maximilian Marx <maximilian.marx@tu-dresden.de>
2018-10-23 20:46:28 +0200

    Add solutions for exercise 01

---
+++ solutions/exercise-01/metis-from-edge-list.py
@@ -0,0 +1,45 @@
+import argparse
+from graphs import Graph
+
+
+def read_edge_list_graph(infile):
+    "parse an edge-list formatted graph from `infile`"
+    num_vertices = int(infile.readline())
+    graph = Graph(range(num_vertices))
+
+    for line in infile:
+        source, target = line.split(' ')
+        graph.add_edge(int(source), int(target))
+
+    return graph
+
+
+def write_metis_graph(outfile, graph):
+    "write `graph` to `outfile`, in METIS format"
+    print('{} {}'.format(graph.number_of_vertices,
+                           graph.number_of_edges),
+          file=outfile)
+
+    for source in graph.vertices:
+        if graph.out_degree(source) > 0:
+            print(source,                # source vertex first
+                  *graph.edges[source],  # then all target vertices
+                  sep=' ',               # separated by a space
+                  file=outfile)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Convert edge-list graph to METIS format")
+    parser.add_argument('input',
+                        help='path to input graph')
+    parser.add_argument('output',
+                        help='path to output graph')
+    args = parser.parse_args()
+
+    graph = None
+    with open(args.input, 'r') as infile:
+        graph = read_edge_list_graph(infile)
+
+    with open(args.output, 'w') as outfile:
+        write_metis_graph(outfile, graph)
