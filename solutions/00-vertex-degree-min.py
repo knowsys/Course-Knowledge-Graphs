d8663d4c7485abdf0c4c8fb3efb09c3e74aaae9d
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-10-30 14:10:44 +0100

    add solutions

---
+++ solutions/00-vertex-degree-min.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+from itertools import islice
+
+def max_degree_vertices(filename):
+    # read the input file
+    with open(filename, 'r') as input_file:
+        vertices = int(input_file.readline())  # first line has the
+                                               # number of vertices
+        degrees = [0] * vertices               # we keep only the
+                                               # degree sequence in
+                                               # memory
+
+        for line in input_file:
+            source, target = line.split(' ')      # for every edge in
+                                                  # the file
+            degrees[int(target)] += 1             # increment the
+                                                  # degree of the
+                                                  # source vertex
+
+    # now we have seen every edge, find the vertices with maximal
+    # degree
+    result = [vertex
+              for (vertex, degree) in enumerate(degrees)
+              if degree == min(degrees)]
+    print(min(degrees), result)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: {} graph.txt'.format(sys.argv[0]))
+
+    max_degree_vertices(sys.argv[1])
