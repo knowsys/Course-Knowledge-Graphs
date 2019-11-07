e5b160c842278a41129ce828e5787a6e7aa6fa00
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-11-07 10:52:25 +0100

    create template directory; add sol 03

--- solutions/03-dblp-linked-data-extraction.py
+++ solutions/03-dblp-linked-data-extraction.py
@@ -5,65 +5,50 @@ import time
 import argparse
 import requests
 
+from time import sleep
+
 from graphs import Graph, io
 
 DBLP_AUTHOR_OF = 'https://dblp.org/rdf/schema-2017-04-18#authorOf'
 DBLP_AUTHORED_BY = 'https://dblp.org/rdf/schema-2017-04-18#authoredBy'
 
 
-"""
-Returns a Graph instance that represents the graph of the NT-file in the DBLP uri.
-@note: in the exercise sheet, there is a commment related to HTTP 429 status code Retry-After.
-This behaviour was changed in DBLP servers.
-@note use time.sleep(2) to avoid requests.exceptions.ConnectionError
-@param (string) iri to be retrieved from dblp
-@returns an instance of Graph
-@hint: use {@code io.read_ntriples_graph} to return an instance of Graph.
-"""
 def _get_ntriples_from_linked_data(iri):
     request = requests.get('{}.nt'.format(iri))
-    time.sleep(1)
-
+    sleep(1)
     if request.status_code == 429:
         delay = int(request.headers['Retry-After'])
         print('got 429, sleeping for {} seconds'.format(delay))
         time.sleep(delay)
         return _get_ntriples_from_linked_data(iri)
 
-    #edit the next line
     return io.read_ntriples_graph(request.text.splitlines())
 
-"""
-(a) get the graph of publications of the author
-(b) check if there is only one author
-(c) return single_autor_papers
-"""
+
 def extract_single_author_papers(author):
     papers = set([])
     single_author_papers = set([])
-
-    #get the graph of the author
     fragment = _get_ntriples_from_linked_data(author)
 
-    # for every (property, object) from author's node
-    # if the predicate is DBLP_AUTHOR_OF then save the paper
     for (prop, obj) in fragment.edges[author]:
+        if prop != DBLP_AUTHOR_OF:
+            continue
 
-        # do something
-        pass
+        papers |= {obj}
 
-    #for every paper
-    #   get the graph of the paper
-    #   if it is a single autor paper, then we save it
     while papers:
         paper = papers.pop()
 
         fragment = _get_ntriples_from_linked_data(paper)
         authors = 0
 
-        # do something
+        for (prop, _) in fragment.edges[paper]:
+            if prop == DBLP_AUTHORED_BY:
+                authors += 1
 
-    #return
+        if authors == 1:
+            single_author_papers |= {paper}
+
     return single_author_papers
 
 
