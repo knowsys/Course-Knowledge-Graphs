102296c74bc7a7a12006b7e8eae5a3494fec448e
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-10-11 15:17:14 +0200

    Add flake, drop old solutions

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Solutions and examples for the Knowledge Graphs course";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "utils/flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, poetry2nix, ... }@inputs:
+    let inherit (utils.lib) mkFlake;
+    in mkFlake {
+      inherit self inputs;
+
+      channels.nixpkgs.overlaysBuilder = channels: [ poetry2nix.overlay ];
+
+      outputsBuilder = channels: {
+        devShell = let
+          pkgs = channels.nixpkgs;
+          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+            overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+              networkit = super.networkit.overridePythonAttrs (old: {
+                nativeBuildInputs = [ self.cython pkgs.cmake ]
+                  ++ (old.nativeBuildInputs or [ ]);
+                buildInputs = [ self.scikit-build ] ++ (old.buildInputs or [ ]);
+                dontUseCmakeConfigure = true;
+              });
+            });
+          };
+        in poetryEnv.env.overrideAttrs
+        (oldAttrs: { buildInputs = [ pkgs.poetry ]; });
+      };
+    };
+}
--- solutions/queries/10.1.3-movie-persons.cypher
+++
@@ -1,3 +0,0 @@
-MATCH (p:Person)-[r1]->(m:Movie)<-[r2]-(p)
-WHERE type(r1)<>type(r2)
-RETURN p.name, m.title
