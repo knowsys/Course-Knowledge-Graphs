e5dd1631ce8d0ca50bac3040c1a7556940122900
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-01-21 18:27:07 +0100

    Move to uv2nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "utils/flake-utils";
-      };
-    };
+
     nemo = {
       url = "github:knowsys/nemo/refs/tags/v0.7.0";
       inputs = {
@@ -18,24 +13,93 @@
         utils.follows = "utils";
       };
     };
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       utils,
-      poetry2nix,
       nemo,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
       ...
     }@inputs:
     let
       inherit (utils.lib) mkFlake;
+      inherit (inputs.nixpkgs) lib;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+      hacks = pkgs: pkgs.callPackage pyproject-nix.build.hacks { };
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      nemoOverlay = pkgs: final: prev: {
+        nemo-python = (hacks final).nixpkgsPrebuilt {
+          from = nemo.packages."${pkgs.system}".nemo-python;
+          prev = prev.requests; # ugly hack, but we require requests anyway
+        };
+      };
+
+      pyprojectOverrides = final: prev: {
+        networkit = prev.networkit.overrideAttrs (old: {
+          nativeBuildInputs = old.nativeBuildInputs ++ [
+            (final.resolveBuildSystem {
+              cmake = [ ];
+              cython = [ ];
+              scikit-build-core = [ ];
+              setuptools = [ ];
+              numpy = [ ];
+            })
+          ];
+        });
+      };
+
+      python = pkgs: pkgs.python312;
+
+      pythonSet =
+        pkgs:
+        (pkgs.callPackage pyproject-nix.build.packages {
+          python = python pkgs;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+              (nemoOverlay pkgs)
+              pyprojectOverrides
+            ]
+          );
+
+      virtualenv =
+        pkgs:
+        (pythonSet pkgs).mkVirtualEnv "knowledge-graphs-venv" (
+          workspace.deps.all // { nemo-python = [ ]; }
+        );
     in
     mkFlake {
       inherit self inputs;
 
       channels.nixpkgs.overlaysBuilder = channels: [
-        poetry2nix.overlays.default
         nemo.overlays.default
       ];
 
@@ -43,40 +107,28 @@
         devShells.default =
           let
             pkgs = channels.nixpkgs;
-            poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-              projectDir = ./.;
-              overrides = pkgs.poetry2nix.overrides.withDefaults (
-                self: super: {
-                  networkit = super.networkit.overridePythonAttrs (old: {
-                    src = pkgs.fetchFromGitHub {
-                      owner = "networkit";
-                      repo = "networkit";
-                      rev = "9b33495752e5b98f1401faea911c026279a0d478";
-                      hash = "sha256-sarzPsbrXpMam16z2kKn53nAW2I99CQ67diaIYLKjCI=";
-                      fetchSubmodules = true;
-
-                    };
-                    nativeBuildInputs = [
-                      self.cython
-                      pkgs.cmake
-                    ] ++ (old.nativeBuildInputs or [ ]);
-
-                    buildInputs = [ self.scikit-build ] ++ (old.buildInputs or [ ]);
-                    dontUseCmakeConfigure = true;
-                  });
-                }
-              );
-              extraPackages = pypkgs: [ pkgs.nemo-python ];
-            };
           in
-          poetryEnv.env.overrideAttrs (oldAttrs: {
-            buildInputs = [
+          pkgs.mkShell {
+            packages = [
+              (virtualenv pkgs)
               pkgs.black
-              pkgs.poetry
               pkgs.mypy
               pkgs.nemo
+              pkgs.uv
             ];
-          });
+            env =
+              {
+                UV_NO_SYNC = "1";
+                UV_PYTHON_DOWNLOADS = "never";
+                UV_PYTHON = python pkgs;
+              }
+              // lib.optionalAttrs pkgs.stdenv.isLinux {
+                LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+              };
+            shellHook = ''
+              unset PYTHONPATH
+            '';
+          };
       };
     };
 }
